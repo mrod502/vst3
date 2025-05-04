0d8f85288110f40ab7f9ac5e1445567e643f0ae9
mrod502 <mrod22502@gmail.com>
2025-05-04 13:08:28 -0400

    feat: initial ADHDSimulator commit

---
+++ src/ADHDSimulator/ADHDSimulator.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <juce_gui_basics/juce_gui_basics.h>
+
+namespace adhd {
+    const juce::String APPLICATION_VERSION = "v0.0.1";
+    const juce::String APPLICATION_NAME = "ADHD Simulator";
+}
